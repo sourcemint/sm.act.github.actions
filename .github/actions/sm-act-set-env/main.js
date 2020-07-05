93e717c2829ac5afc7025e30f7937ce83e7fce16
Christoph Dorn <christoph@christophdorn.com>
2020-07-04 22:13:59 -0700

    wip

--- .github/actions/sm-act-set-env/main.js
+++ .github/actions/sm-act-set-env/main.js
@@ -10,15 +10,18 @@ function setEnv (name, value) {
 }
 
 
+setEnv('SM_ACT_REPO_URI', `github.com/${getEnv('GITHUB_REPOSITORY')}`);
 setEnv('SM_ACT_GIT_REMOTE', `git@github.com:${getEnv('GITHUB_REPOSITORY')}.git`);
 setEnv('SM_ACT_GIT_REF', getEnv('GITHUB_REF'));
 setEnv('SM_ACT_GIT_SHA', getEnv('GITHUB_SHA'));
 setEnv('SM_ACT_GIT_SHA7', getEnv('GITHUB_SHA').substr(0, 7));
-setEnv('SM_ACT_RUN_ID', getEnv('GITHUB_RUN_ID'));
+setEnv('SM_ACT_RUN_ID', `github-actions-${getEnv('GITHUB_RUN_ID')}`);
+setEnv('SM_ACT_NAME', getEnv('GITHUB_JOB'));
+
 setEnv('SM_ACT_ACTOR_URI', `github.com/${getEnv('GITHUB_ACTOR')}`);
 setEnv('SM_ACT_TRIGGER_EVENT', getEnv('GITHUB_JOB'));
-setEnv('SM_ACT_JOB_NAME', getEnv('GITHUB_JOB'));
-setEnv('SM_ACT_PACKAGE_URI', `github.com/${getEnv('GITHUB_REPOSITORY')}`);
+
+setEnv('SM_ACT_COMPONENT_ID', `/`);     // '/' denotes repository-wide component (otherwise a path to root of component)
 
 if (/^refs\/tags\//.test(getEnv('GITHUB_REF'))) {
     setEnv('SM_ACT_GIT_TAG', getEnv('GITHUB_REF').replace(/^refs\/tags\//, ''));
@@ -30,11 +33,13 @@ if (/^refs\/heads\//.test(getEnv('GITHUB_REF'))) {
 
 const time = MOMENT();
 setEnv('SM_ACT_SNAPSHOT_ID', [
-    time.format('YYMM'),
-    getEnv('SM_ACT_PACKAGE_URI'),
+    time.format('YY-MM-DD'),
+    getEnv('SM_ACT_REPOSITORY_URI'),
+    getEnv('SM_ACT_COMPONENT_ID'),
     getEnv('SM_ACT_GIT_SHA7'),
-    getEnv('SM_ACT_RUN_ID'),
     time.format('DD-HHmm-ss'),
     getEnv('SM_ACT_GIT_BRANCH'),
-    getEnv('SM_ACT_ACTOR_URI')
+    getEnv('SM_ACT_NAME'),
+    getEnv('SM_ACT_ACTOR_URI'),
+    getEnv('SM_ACT_RUN_ID')
 ].join(':'));
